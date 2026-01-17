b3ac0d765f635fbd3b072d2d7789c292d439d3e1
Augustine59-Wangombe <wangombeaugustine58@gmail.com>
2026-01-17 12:42:13 -0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,26 @@
+
+import { serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  doc, 
+  setDoc, 
+  query, 
+  where, 
+  getDocs 
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw90VniQ8yVJiIPAAXiXO3qA_kpGDrz6w",
+  authDomain: "registrations-5594b.firebaseapp.com",
+  projectId: "registrations-5594b",
+  storageBucket: "registrations-5594b.firebasestorage.app",
+  messagingSenderId: "811011800366",
+  appId: "1:811011800366:web:dd3c191aa20f8bc43a0ebf",
+  measurementId: "G-YVRZ1WHD8R"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db, collection, serverTimestamp, addDoc, query, where, getDocs };  
